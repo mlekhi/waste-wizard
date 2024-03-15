8f4f22f3fcd48dfc8cbd45da40f999499517718a
Matthew Alexander Toldo <mtoldo2@uwo.ca>
2024-03-14 23:03:52 -0400

    Create Account page template made

---
+++ client/src/pages/CreateAccount/CreateAccount.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import './CreateAccount.css';
+
+function CreateAccount() {
+  return (
+    <div clasName="App">
+      <h1>Create an Account</h1>
+      <div className="Info">
+      <div className="Spacing">
+      <div className="Input">
+        <p>Name: </p>
+        <input type="text" id="InfoBox"/>
+        </div>
+        <div className="Input">
+        <p>User ID: </p>
+        <input type="text" id="InfoBox"/>
+        </div>
+        <div className="Input">
+        <p>Password:</p>
+        <input type="text" id="InfoBox"/>
+        </div>
+        <div className="Input">
+        <p>Re-type password:</p>
+        <input type="text" id="InfoBox"/>
+        </div>
+        
+        </div>
+        <div className="ButtonBounds">
+        <button class = "WWButton">Become a Waste Wizard</button>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default CreateAccount;
