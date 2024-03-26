0007337b13a34d00dfe4b4e13217390761be3c7e
Matthew Alexander Toldo <mtoldo2@uwo.ca>
2024-03-26 19:55:53 -0400

    fixing merge problems

--- client/src/App.js
+++ client/src/App.js
@@ -1,31 +1,36 @@
 import './App.css';
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import Play from './pages/Play';
+import Play from './pages/Play/Play';
+import Multiplayer from './pages/Play/Multiplayer';
 import Default from './pages/Default';
-import Debug from './pages/Debug';
+import Debug from './pages/Debug/Debug';
 import Login from './pages/Login/Login';
 import Shop from './pages/Shop';
 import Instructor from './pages/Instructor';
-import Leaderboard from './pages/Leaderboard';
-import Tutorial from './pages/Tutorial';
+import Leaderboard from './pages/Leaderboard/Leaderboard';
+import Tutorial from './pages/Tutorial/Tutorial';
+import CreateAccount from './pages/CreateAccount/CreateAccount';
+import Menu from './components/Menu/Menu';
 
 function App() {
   return (
     <div className="App">
-      <p>Waste Wizard</p>
+      <Menu />
       <Router>
-        <div>
+        <div className="content">
           <Routes>
-            <Route exact path="*" component={Default} />
-            <Route exact path="/Default" component={Default} />
-            <Route exact path="/Play" component={Play} />
-            <Route exact path="/Debug" component={Debug} />
-            <Route exact path="/Login" element={<Login/>} />
-            <Route exact path="/Shop" component={Shop} />
-            <Route exact path="/Instructor" component={Instructor} />
-            <Route exact path="/Leaderboard" component={Leaderboard} />
-            <Route exact path="/Tutorial" component={Tutorial} />
+            <Route exact path="*" element={<Default />} />
+            <Route exact path="/Default" element={<Default />} />
+            <Route exact path="/Play" element={<Play />} />
+            <Route exact path="/Multiplayer" element={<Multiplayer />} />
+            <Route exact path="/Debug" element={<Debug />} />
+            <Route exact path="/Login" element={<Login />} />
+            <Route exact path="/Shop" element={<Shop />} />
+            <Route exact path="/Instructor" element={<Instructor />} />
+            <Route exact path="/Leaderboard" element={<Leaderboard />} />
+            <Route exact path="/Tutorial" element={<Tutorial />} />
+            <Route eaxct path="/CreateAccount" element={<CreateAccount />} />
           </Routes>
         </div>
       </Router>
