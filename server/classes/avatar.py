624a4dd22d26dd7b9106154cd0037d9cbb607315
Marianna Nikko Speranza <msperan4@uwo.ca>
2024-04-02 03:25:47 +0000

    avatar.py edited online with Bitbucket

--- server/classes/avatar.py
+++ server/classes/avatar.py
@@ -1,13 +1,14 @@
-"""
-Represents an avatar, including its ID, cost, and name.
 
-Attributes:
-    avatarID (int): The unique identifier for the avatar.
-    cost (int): The cost associated with acquiring the avatar.
-    name (str): The name of the avatar.
-"""
 
 class Avatar:
+    """
+    Represents an avatar, including its ID, cost, and name.
+
+    Attributes:
+        avatarID (int): The unique identifier for the avatar.
+        cost (int): The cost associated with acquiring the avatar.
+        name (str): The name of the avatar.
+    """
     """
     Initializes a new instance of the Avatar class.
     """
@@ -16,22 +17,24 @@ class Avatar:
         self._cost = cost  # integer
         self._name = name  # boolean
 
+  
+    def get_cost(self):
     """
     Gets the cost of the avatar
 
     Returns:
         int: The cost of the avatar
     """
-    def get_cost(self):
         return self._cost
 
+  
+    def get_name(self):
     """
     Gets the name of the avatar
 
     Returns:
         str: The name of the avatar
     """
-    def get_name(self):
         return self._name
 
     def get_avatarID(self):
